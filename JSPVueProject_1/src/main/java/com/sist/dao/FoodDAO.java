ded04b7abbc760c4fd7a72ffa28376021eee1004
fhzmx2-blip <fhzmx2@gmail.com>
2026-06-24 17:49:23 +0900

    2026-06-24

--- JSPVueProject_1/src/main/java/com/sist/dao/FoodDAO.java
+++ JSPVueProject_1/src/main/java/com/sist/dao/FoodDAO.java
@@ -79,4 +79,32 @@ public class FoodDAO {
     	session.close();
     	return vo;
     }
+    /*
+     *   <select id="foodFindListData" resultType="FoodVO" parameterType="hashmap">
+		    SELECT no,poster,name,address
+		    FROM food
+		    WHERE ${column} LIKE '%'||#{ss}||'%'
+		    OFFSET #{start} ROWS FETCH NEXT 12 ROWS ONLY
+		  </select>
+     */
+    public static List<FoodVO> foodFindListData(Map map)
+    {
+   	 SqlSession session=ssf.openSession();
+   	 List<FoodVO> list=session.selectList("foodFindListData",map);
+   	 session.close();
+   	 return list;
+    }
+    /*
+     *   <select id="foodFindTotalPage" resultType="int" parameterType="hashmap">
+		    SELECT CEIL(COUNT(*)/12.0) FROM food
+		    WHERE ${column} LIKE '%'||#{ss}||'%'
+		  </select>
+     */
+    public static int foodFindTotalPage(Map map)
+    {
+    	SqlSession session=ssf.openSession();
+    	int total=session.selectOne("foodFindTotalPage",map);
+    	session.close();
+    	return total;
+    }
 }
